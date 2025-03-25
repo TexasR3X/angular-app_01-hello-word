aaa09e918067333be79007cd817b985816c2cfa6
Texas R3X <texasr3xphone@gmail.com>
2025-03-25 09:03:27 -0600

    HomeComponent added; *.component.html files added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: `
-    <h1>Default</h1>
-  `,
-  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent],
+  templateUrl: "./app.component.html",
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'default';
+  title = 'homes';
 }
